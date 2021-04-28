9a31bda594432faecdbc8382ddabf875a1ed6cf0
sreejith4 <77230940+sreejith4@users.noreply.github.com>
2021-04-28 18:36:55 +0530

    Add files via upload

---
+++ addressbook_selenium_ide/src/main/java/com/mycompany/app/addressbook_selenium_ide/App.java
@@ -0,0 +1,42 @@
+package com.mycompany.app.addressbook_selenium_ide;
+
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.setProperty("webdriver.chrome.driver","C:\\Edureka\\chromedriver.exe");
+        ChromeOptions chromeOptions= new ChromeOptions();
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        
+        System.out.println("Welcome to demo for selenium chrome webdriver");
+        
+        driver.get("http://13.235.78.97:8888/");
+        
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        driver.findElement(By.className("v-button")).click();
+          driver.findElement(By.id("gwt-uid-5")).sendKeys("Sreejith4edureka2");
+          driver.findElement(By.id("gwt-uid-7")).sendKeys("Devops2");
+          driver.findElement(By.id("gwt-uid-9")).sendKeys("8861679781");
+          driver.findElement(By.id("gwt-uid-11")).sendKeys("Sreejith4edureka22@edureka.com");
+          driver.findElement(By.id("gwt-uid-13")).sendKeys("01/02/2020");
+       driver.findElement(By.className("v-button-primary")).click();
+       //Thread.sleep(5000);
+       //driver.quit();
+       
+         
+    }
+}
